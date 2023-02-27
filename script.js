82e0fb7f46f738bee5dba0692a74f602e8bf1d71
Itsayu <ayushkr9976@gmail.com>
2023-02-27 07:48:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
